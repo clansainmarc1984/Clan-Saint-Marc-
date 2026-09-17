93673abb75fbc98cc47a077bb44890ddcd108049
clansainmarc1984 <clansaintmarc1984@gmail.com>
2026-09-17 22:34:53 +0100

    Update Supabase anonymous key in config.js

--- config.js
+++ config.js
@@ -2,4 +2,3 @@
 // Remplace seulement les deux valeurs ci-dessous par celles de ton projet Supabase.
 window.SUPABASE_URL ="https://lvjlpqzgaoktbihbbqme.supabase.co";
 window.SUPABASE_ANON_KEY ="sb_publishable_SdnS4zzVO4l5E3R93WiDxg_5hJwvfVr";
-
