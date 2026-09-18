b284f6032e82d170f13b99cf9da3fcd9fc34989a
clansainmarc1984 <clansaintmarc1984@gmail.com>
2026-09-18 16:13:32 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // Configuration Supabase du Clan Saint Marc.
 // Remplace seulement les deux valeurs ci-dessous par celles de ton projet Supabase.
-window.SUPABASE_URL ="https://lvjlpqzgaoktbihbbqme.supabase.co";
-window.SUPABASE_ANON_KEY ="sb_publishable_SdnS4zzVO4l5E3R93WiDxg_5hJwvfVr";
+window.SUPABASE_URL =
+  "https://lvjlpqzgaoktbihbbqme.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_SdnS4zzVO4l5E3R93WiDxg_5hJwvfVr";
