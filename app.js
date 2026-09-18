528b4837848d17562c9fc8ec7f73c2f00222967a
clansainmarc1984 <clansaintmarc1984@gmail.com>
2026-09-18 13:57:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let data = structuredClone(emptyData);
 let supabaseClient = null;
 const TABLES = Object.keys(emptyData);
 
-function configured(){ return window.SUPABASE_URL && window.SUPABASE_ANON_KEY && !window.SUPABASE_URL.includes("https://lvjlpqzgaoktbihbbqme.supabase.co") && !window.SUPABASE_ANON_KEY.includes('sb_publishable_SdnS4zzVO4l5E3R93WiDxg_5hJwvfVr'); }
+function configured(){ return window.SUPABASE_URL && window.SUPABASE_ANON_KEY}
 function initDb(){
   if(configured() && window.supabase){ supabaseClient = window.supabase.createClient(window.SUPABASE_URL, window.SUPABASE_ANON_KEY); }
 }
